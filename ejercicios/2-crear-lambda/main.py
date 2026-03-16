eea4ba306bb40336533a266dd8407bb406a16816
Andrés Perdomo <alvarado0925@gmail.com>
2026-03-16 14:50:20 -0500

    Agregar manejo de eventos de DynamoDB en la función Lambda

--- ejercicios/2-crear-lambda/main.py
+++ ejercicios/2-crear-lambda/main.py
@@ -1,6 +1,30 @@
-import json
-
-
 def dynamodb_events(event, context):
-    print("DynamoDB event received:")
-    print(json.dumps(event, indent=2, default=str))
+    try:
+        for record in event["Records"]:
+            if record["eventName"] == "MODIFY":
+                manejar_update(record)
+            elif record["eventName"] == "INSERT":
+                manejar_insert(record)
+            elif record["eventName"] == "REMOVE":
+                manejar_remove(record)
+
+    except Exception as e:
+        print("❌ Error processing event:", e)
+        raise e
+
+    return "✅ Event processed successfully."
+
+
+def manejar_update(record):
+    print("🔄 Update event detected:")
+    print(record)
+
+
+def manejar_insert(record):
+    print("➕ Insert event detected:")
+    print(record)
+
+
+def manejar_remove(record):
+    print("➖ Remove event detected:")
+    print(record)
